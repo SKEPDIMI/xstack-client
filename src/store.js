85b21b695cdf179991eff346b9266671dd55ad5c
John S <diaz.john312@gmail.com>
2018-10-21 13:43:58 -0400

    vuex store auth

--- src/store.js
+++ src/store.js
@@ -1,29 +1,52 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import store from 'store';
 
 Vue.use(Vuex);
 
 const defaultUser = {
-  email: '',
+  email: null,
   id: null,
   uid: null,
+  image: {},
+  role: 0,
+  bio: '',
+  description: '',
+
   authorized: false,
   loading: true,
+  token: null
 }
 
 const currentUser = {
+  namespaced: true,
   state: {...defaultUser},
   mutations: {
-    setDefault: state => ({
-      ...defaultUser,
-      loading: false
-    }),
-    setUser: (state, user) => ({
-      ...defaultUser,
-      ...user,
-      loading: false,
-      authorized: true
-    })
+    setDefault: state => {
+      state = {
+        ...defaultUser,
+        loading: false
+      }
+    },
+    setUser: (state, user) => {
+      state = {
+        ...defaultUser,
+        ...user,
+        loading: false,
+        authorized: true
+      }
+    },
+    setTokenFromHeaders: (state, headers) => {
+      state.token = {
+        'access-token': headers['access-token'],
+        'client': headers['client'],
+        'uid': headers['uid'],
+        'expiry': headers['expiry'],
+        'token-type': headers['token-type'],
+      }
+
+      store.set('DVSE_XSTK_AUTH', state.token)
+    }
   }
 }
 
