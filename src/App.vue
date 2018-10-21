a2c6e6b81581157786d507ec12ff8df7930581c2
John S <diaz.john312@gmail.com>
2018-10-21 13:44:59 -0400

    login on app mount

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,25 @@
 import Header from '@/components/Header';
 import Sidemenu from '@/components/Sidemenu';
 import Footer from '@/components/Footer';
+import api from './api';
+import store from 'store';
 
 export default {
   name: 'App',
   mounted() {
-    console.log('hello, world! app has started!')
-    console.log(this.$store)
+    let tokenHeaders = store.get('DVSE_XSTK_AUTH');
+
+    if(tokenHeaders) {
+      api.get('/auth/validate_token', tokenHeaders)
+      .then(res => {
+        if (res.ok) {
+          this.$store.commit('currentUser/setUser', res.body.data);
+          this.$store.commit('currentUser/setTokenFromHeaders', res.headers);
+        } else {
+          store.remove('DVSE_XSTK_AUTH')
+        }
+      });
+    }
   },
   components: {
     Header,
