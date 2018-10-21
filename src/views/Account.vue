474f1d356299d6bc5a2812297b8fa4a8bbb666cd
John S <diaz.john312@gmail.com>
2018-10-21 17:31:13 -0400

    basic account view

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,25 +1,66 @@
-<template v-if="this.$route.path == '/me'">
-  <div class="account">
-    <div>
-      <h1>Welcome, user!</h1>
+<template>
+  <!-- OWNER OF ACCOUNT -->
+  <div v-if="ownAccount">
+    <div class="account" v-if="currentUser.authenticated">
+      <div class="header">
+        <h1>My Account</h1>
+        <img :src="currentUser.img.url || '@/assets/icon/profile_image.png'" alt="profile picture">
+      </div>
+      <hr>
+      <p class="mute">Joined on {{currentUser.created_at}}</p>
+      <div class="body">
+        <h1>Email:</h1>
+        <p>{{currentUser.email}}</p>
+        <h1>Bio:</h1>
+        <p>{{currentUser.bio.length}}</p>
+        <h1>Description:</h1>
+        <p>{{currentUser.description}}</p>
+      </div>
+    </div>
+    <div v-else class="account">
+      <h1>Not logged in</h1>
+      <hr>
+      <p class="mute">Please <router-link to="/login">log in</router-link> to view your account</p>
     </div>
   </div>
+  <!-- VIEWING OTHER ACCOUNT -->
+  <div v-else class="account">
+    <h1>wat</h1>
+  </div>
 </template>
-<template v-else="">
-  
-</template>
-
 
 <script>
 export default {
   mounted() {
     // fetch user if not self
+  },
+  computed: {
+    ownAccount() {
+      return this.$route.path == '/me'
+    },
+    currentUser() {
+      return this.$store.state.currentUser
+    }
   }
 }
 </script>
 
 <style>
 .account {
-  
+  background-color: white;
+  min-height: 90vh;
+  text-align: left;
+  padding: 10px 20px;
+}
+.account .header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.account .header img {
+  height: 4em;
+  width: 4em;
+  border-radius: 2em;
 }
 </style>
